6e9c1b6460156d7827425d403b12f1e0fce09064
James McCollum <jmccollum@roydan.com>
2017-06-23 07:50:49 -0500

    Additional fixes for windows/git 2.0

--- lib/svn2git/migration.rb
+++ lib/svn2git/migration.rb
@@ -188,7 +188,7 @@ module Svn2Git
         if nominimizeurl
           cmd += "--no-minimize-url "
         end
-        cmd += "--trunk='#{@url}'"
+        cmd += "--trunk=#{@url}"
         run_command(cmd, true, true)
 
       else
@@ -338,7 +338,7 @@ module Svn2Git
         if @options[:rebase] && (@local.include?(branch) || branch == 'trunk')
            lbranch = branch
            lbranch = 'master' if branch == 'trunk'
-           run_command("git checkout -f -b \"#{lbranch}\"")
+           run_command("git checkout -f \"#{lbranch}\"")
            run_command("git rebase \"remotes/svn/#{branch}\"")
            next
         end
@@ -371,7 +371,7 @@ module Svn2Git
 
             @legacy_svn_branch_tracking_message_displayed = true
 
-            run_command("git checkout -b \"#{branch}\" \"remotes/svn/#{branch}\"")
+            run_command("git checkout \"#{branch}\"")
           end
         end
       end
@@ -384,7 +384,7 @@ module Svn2Git
         run_command("git branch -D master")
         run_command("git checkout -f -b master")
       else
-        run_command("git checkout -f -b master")
+        run_command("git checkout -f master")
       end
     end
 
